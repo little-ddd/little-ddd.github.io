08ab156f2045a2bf0dc2998367094b0a6c2d397a
zsan3653 <zsan3653@gmail.com>
2024-03-18 15:33:03 +0800

    修改 label

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -133,7 +133,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Little-DDD, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
